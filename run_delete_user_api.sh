13570c6aaec4722742aa41d5ec2f0060f2ba839a
Jerry Lin <jerry.lin.bccs@gmail.com>
2021-06-01 22:55:34 -0400

    change test time

--- run_delete_user_api.sh
+++ run_delete_user_api.sh
@@ -4,4 +4,4 @@ DATE=`date +%Y_%m%d_%H%M%S`
 mkdir -p result/${DATE}
 
 
-locust  -f delete_user_api_test.py  --headless -u 2  -r 1  -t6m  --csv="result/${DATE}/delete_user_api_test_data"  --html="result/${DATE}/delete_user_api_test.html"  --logfile="logs/delete_user_api_test_${DATE}.log"
+locust  -f delete_user_api_test.py  --headless -u 2  -r 1  -t3m  --csv="result/${DATE}/delete_user_api_test_data"  --html="result/${DATE}/delete_user_api_test.html"  --logfile="logs/delete_user_api_test_${DATE}.log"
